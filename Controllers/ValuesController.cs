69fe8265054fc4e7f72e91d578e91061e60c4e74
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2018-10-25 22:29:11 +0300

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JB.Inspection.EditorConfig.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void ResharperPossibleNullReferenceExceptionHighlighting([FromBody] object obj)
+		{
+			if (obj != null)
+			{
+				Console.WriteLine(obj.ToString());
+			}
+
+			Console.WriteLine(obj.GetHashCode());
+		}
+    }
+}
